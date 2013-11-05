07209366cb0415a2a7c7d461945c1b060abedcfd
noxan <noxan@byteweaver.net>
2013-11-05 22:09:43 +0100

    add some more hard coded django projects for testing

--- projects/client/views/projectOverview.js
+++ projects/client/views/projectOverview.js
@@ -1,3 +1,15 @@
 Template.projectOverview.projects = function() {
-  return [{'name': 'django-posts'}, {'name': 'django-forums'}, {'name': 'django-referral'}];
+  return [
+    {'name': 'django-posts'},
+    {'name': 'django-forums'},
+    {'name': 'django-referral'},
+    {'name': 'django-coupons'},
+    {'name': 'django-tickets'},
+    {'name': 'django-skrill'},
+    {'name': 'django-pantone'},
+    {'name': 'django-polls'},
+    {'name': 'django-singleactiveobject'},
+    {'name': 'django-eca-catalogue'},
+    {'name': 'django-skeleton'},
+  ];
 }
